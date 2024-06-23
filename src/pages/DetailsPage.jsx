1f03249113cb2316a6dd77c0dddc7161e15dda90
Jalen Hudson <JalenProgramming@gmail.com>
2024-06-23 19:05:47 -0400

    Updated details page

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -40,10 +40,10 @@ const DetailsPage = () => {
       <Link className="return-home-btn" to={"/invoice-app-vite/"}>
        <img src={ArrowLeft} alt="" srcset="" /> Go back
       </Link>
-          <div className="status-info-bar">
-            <div className="status-info">
+          <div className="detail-status-info-bar">
+            <div className="detail-status-info">
 
-            <span className="info-status">Status</span>{" "}
+            <span className="detail-info-status">Status</span>{" "}
             <p
               className={`${
                 invoiceDetail.status === "paid"
@@ -73,8 +73,73 @@ const DetailsPage = () => {
             <Button className={"ivoice-detail-paid-btn"} buttonText={"Mark as Paid"} />
             </div>   
           </div>
-          <h3>Invoice Details:</h3>
-          <p>{invoiceDetail.id}</p>
+
+          <div className="detail-invoice-info">
+            <div className="detail-id-desc-billfrom">
+              <div>
+              <p className="detail-info secondary"><span className="detail-id-hash">#</span>{invoiceDetail.id}</p>
+              <p className="detail-info primary">{invoiceDetail.description}</p>
+              </div>
+              <div>
+                <p className="detail-info primary">{invoiceDetail.senderAddress.street}</p>
+                <p className="detail-info primary">{invoiceDetail.senderAddress.city}</p>
+                <p className="detail-info primary">{invoiceDetail.senderAddress.postCode}</p>
+                <p className="detail-info primary">{invoiceDetail.senderAddress.country}</p>
+
+              </div>
+            </div>
+            <div className="detail-invDate-billToName-billToEmail">
+                <div>
+                  <p className="detail-head primary">Invoice Date</p>
+                  <p className="detail-info secondary">{invoiceDetail.createdAt}</p>
+                </div>
+                <div>
+                  <p className="detail-head primary">Bill To</p>
+                  <p className="detail-info secondary">{invoiceDetail.clientName}</p>
+                </div>
+                <div>
+                  <p className="detail-head primary">Sent to</p>
+                  <p className="detail-info secondary">{invoiceDetail.clientEmail}</p>
+                </div>                                
+              </div>
+
+              <div className="detail-payDue-billToAddr">
+                <div>
+                  <p className="detail-head primary">Payment Due</p>
+                  <p className="detail-info secondary">{invoiceDetail.paymentDue}</p>
+                </div>
+                <div>
+                <p className="detail-info primary">{invoiceDetail.clientAddress.street}</p>
+                <p className="detail-info primary">{invoiceDetail.clientAddress.city}</p>
+                <p className="detail-info primary">{invoiceDetail.clientAddress.postCode}</p>
+                <p className="detail-info primary">{invoiceDetail.clientAddress.country}</p>
+                </div>                              
+              </div>
+
+              <div className="items-container">
+                <div className="item-header">
+                  <p className="item-header-name">Item Name</p>
+                  <p className="item-header-qty">QTY.</p>
+                  <p className="item-header-price">Price</p>
+                  <p className="item-header-total">Total</p>
+                </div>
+
+                {invoiceDetail.items.map((item, index) => (
+                <div key={index} className="item-row">
+                  <p className="item-name">{item.name}</p>
+                  <p className="item-quantity">{item.quantity}</p>
+                  <p className="item-price">{item.price.toFixed(2)}</p>
+                  <p className="item-total">{item.total.toFixed(2)}</p>
+                </div>
+              ))}
+              </div>
+
+              <div className="amount-total">
+                <p className="amount-due">Amount Due</p>
+                <p className="total">$ {invoiceDetail.total.toFixed(2)}</p>
+              </div>
+          </div>
+
         </section>
       ) : (
         <p>Loading...</p>
